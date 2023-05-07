a724c2f0788704d1258ff566329784c42d5e2648
wizock <silent.death3500@gmail.com>
2023-05-08 04:27:31 +1000

    setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="BlockFrame",
-    version="0.1.5",
+    version="1.0.0",
     author="Rohaan Ahmed",
     author_email="silent.death3500@gmail.com",
     description="File Chunking Library to work as a data-store solution alongside webapps and software.",
